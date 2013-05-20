c0bfc81c011812797d12b5ba096b760d3e4f244d
Bobby Matson <bobby.matson@gmail.com>
2013-05-20 16:31:06 -0700

    notes on process improvement

---
+++ tips/process_improvements.rb
@@ -0,0 +1,7 @@
+#later stage, for testing against Ruby core lib:
+
+#write a module, BobbyEnumerable, that implements #bobby_map, #bobby_reduce
+
+#Implement your own versions of map and reduce
+
+#include it in your own class, implement the each method
