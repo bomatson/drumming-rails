371308798cdfba1aeeb656530de6f7565f0bacdb
Bobby Matson <bobby.matson@gmail.com>
2013-07-30 14:52:57 -0700

    cleanup

--- mini_projects/typing/client.rb
+++ mini_projects/typing/client.rb
@@ -3,10 +3,10 @@ require 'json'
 
 SUCCESSES = []
 FAILURES = []
+
 #rackup config.ru --port=4567
 conn = Faraday.new(:url => 'http://0.0.0.0:4567') do |c|
   c.use Faraday::Request::UrlEncoded  # encode request params as "www-form-urlencoded"
-  # c.use Faraday::Response::Logger     # log request & response to STDOUT
   c.use Faraday::Adapter::NetHttp     # perform requests with Net::HTTP
 end
 
@@ -73,7 +73,6 @@ def start_game(user, conn, difficulty = {})
   
   JSON.parse(response.body).each do |word|
     puts "#{word}"
-  	# puts "#{user}, this is word #{id}"
     %x(say -v Victoria #{word})
 
     evaluate(word)
