1ac7a70d5dd47969d76861509d73df64a0a0b68e
bomatson <bobby@carbonfive.com>
2013-08-27 10:19:30 -0700

    reformat test structure and work through creation examples

--- spec_the_std_lib/rspec/hash_spec.rb
+++ spec_the_std_lib/rspec/hash_spec.rb
@@ -1,18 +1,28 @@
 require "rubygems"
 require "rspec"
 
-describe 'Hash' do
+describe Hash do
 	let(:hash) {{a: "a", b: "b"}}
 
-  it 'example is an instance of Hash' do
-    expect(hash).to be_instance_of(Hash)
+  it 'is an instance of Hash' do
+    expect(hash).to be_instance_of Hash
   end
 
-  it 'example is a kind of Hash' do
-    expect(hash).to be_kind_of(Hash)
+  it 'is a kind of Hash' do
+    expect(hash).to be_kind_of Hash
   end
 
-  it 'will use literal Hash syntax to create a new hash' do
-    expect(Hash['a', 100, 'b', 200]).to eq({'a' => 100, 'b' => 200})
+  context 'can be created' do
+    it 'using literal Hash syntax' do
+      expect(Hash['a', 100, 'b', 200]).to eq({'a' => 100, 'b' => 200})
+    end
+
+    it 'using an implicit form' do
+      expect({ Robert: 25,  Nicolle: 26 }).to eq({:Robert=>25, :Nicolle=>26})
+    end
+
+    it 'uses the new method' do
+      expect(Hash.new).to be_kind_of Hash
+    end
   end
 end
