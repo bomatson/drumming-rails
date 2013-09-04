f56df2e222df234963ffd934a2ca99ba4cf37149
Bobby Matson <bobby.matson@gmail.com>
2013-09-03 19:26:27 -0700

    add in delete and clear methods

--- spec_the_std_lib/rspec/hash_spec.rb
+++ spec_the_std_lib/rspec/hash_spec.rb
@@ -12,7 +12,7 @@ describe Hash do
     expect(hash).to be_kind_of Hash
   end
 
- context 'can be created' do
+  context 'can be created' do
     it 'using literal Hash syntax' do
       expect(Hash['a', 100, 'b', 200]).to eq({'a' => 100, 'b' => 200})
     end
@@ -39,22 +39,51 @@ describe Hash do
     end
   end
 
- context 'accesses its values' do
-   it 'using a symbol reference if the key is a symbol' do
+  context 'accesses its values' do
+    it 'using a symbol reference if the key is a symbol' do
      expect(hash[:a]).to eq 1234
-   end
+    end
 
-   it 'without using a string reference if the key is a symbol' do
+    it 'without using a string reference if the key is a symbol' do
      expect(hash['a']).to_not eq 1234
-   end
+    end
 
-   it 'using the values method' do
+    it 'using the values method' do
      expect(hash.values).to eq [1234, 'string']
-   end
+    end
 
-   it 'with the ability to override the value' do
+    it 'with the ability to override the value' do
      hash[:a] = 123
      expect(hash[:a]).to eq 123
-   end
- end
+    end
+
+    it 'with the ability to set a new key and value' do
+     hash[:c] = 'awesome'
+     expect(hash[:c]).to eq 'awesome'
+    end
+  end
+
+  context 'uses equality to compare hashes' do
+    let(:identical) {{a: 1234, b: 'string'}}
+   
+    it 'with the == operator' do
+     expect(identical == hash).to be true
+    end
+  end
+
+  context 'manipulates its objects' do
+    
+    it 'with clear to empty the hash' do
+      expect(hash.clear).to be_empty
+    end
+
+    it 'with delete to remove an element by its key' do
+      hash.delete(:a)
+      expect(hash).to_not include 1234
+    end
+
+    it 'with delete to return nil if key is not present' do 
+      expect(hash.delete(:z)).to be_nil
+    end
+  end
 end
