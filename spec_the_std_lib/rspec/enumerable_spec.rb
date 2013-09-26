f73c7268c9c3a3979eb9fdc35c5005c3eddb7555
bomatson <bobby@carbonfive.com>
2013-09-26 10:24:47 -0700

    finish off all for Enumerable

--- spec_the_std_lib/rspec/enumerable_spec.rb
+++ spec_the_std_lib/rspec/enumerable_spec.rb
@@ -8,6 +8,10 @@ describe Enumerable do
           collection.all? { |word| word.length < 10 }
         ).to eq true
       end
+
+      it 'all? without a block will implicitly return true if no collection items are nil or false' do
+        expect(collection.all?).to be_true
+      end
     end
   end
 end
