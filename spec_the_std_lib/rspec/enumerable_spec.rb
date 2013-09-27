06a768510e3023e1fe9642f2eeeeabe967ba59f9
bomatson <bobby@carbonfive.com>
2013-09-27 10:29:25 -0700

    spec for any? in Enumerable

--- spec_the_std_lib/rspec/enumerable_spec.rb
+++ spec_the_std_lib/rspec/enumerable_spec.rb
@@ -9,8 +9,24 @@ describe Enumerable do
         ).to eq true
       end
 
+      it 'all? returns false if the block ever returns false or nil' do
+        expect(
+          collection.all? { |word| word.length < 9 }
+        ).to eq false
+      end
+
       it 'all? without a block will implicitly return true if no collection items are nil or false' do
         expect(collection.all?).to be_true
+      end
+
+      it 'any? returns true if the block ever returns a value other than false or nil' do
+        expect(
+          collection.any? { |word| word.length < 10 }
+        ).to eq true
+
+        expect(
+          collection.any? { |word| word.length == 3 }
+        ).to eq true
       end
     end
   end
