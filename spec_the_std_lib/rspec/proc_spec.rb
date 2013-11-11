60dee78d1265bb7abddd3cd51d54709ed54f4620
bomatson <bobby@carbonfive.com>
2013-11-11 10:16:56 -0800

    arity and binding

--- spec_the_std_lib/rspec/proc_spec.rb
+++ spec_the_std_lib/rspec/proc_spec.rb
@@ -25,5 +25,21 @@ describe Proc do
     it '[] invokes the block with params' do
       expect( my_proc[1,2] ).to be_true
     end
+
+    it 'arity returns number of args that would be used in a proc with finite args' do
+      expect(my_proc.arity).to eq 1
+    end
+
+    it 'arity returns mandatory args as -n - 1 if optional args in the block' do
+      expect(
+        proc{ |*a| }.arity
+      ).to eq -1
+    end
+
+    it 'binding returns the execution context retained for future use'  do
+      expect(
+        my_proc.binding
+      ).to be_kind_of Binding
+    end
   end
 end
