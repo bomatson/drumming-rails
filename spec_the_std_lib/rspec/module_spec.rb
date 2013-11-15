02d69da62d6b92b137ce46e73513db6f3fc3c6c3
bomatson <bobby@carbonfive.com>
2013-11-15 13:50:11 -0800

    equality and ancestry for modules

--- spec_the_std_lib/rspec/module_spec.rb
+++ spec_the_std_lib/rspec/module_spec.rb
@@ -38,13 +38,65 @@ describe Module do
     end
 
     describe 'public #instance methods' do
-      context 'given a Module with a constant and instance methods' do
+      context 'given a module with a constant and instance methods' do
+        let!(:subclass) { class Games < Module ; end }
+        Battles = Module.new
+
         module War
+          include Battles
+
           ARMY = 1
 
           def begin
             puts 'war started!'
           end
+        end
+
+
+        it '< will return true if module is subclass of other' do
+          expect(
+            Games < Module
+          ).to be_true
+        end
+
+        it '< will return nil if there is no relationship between the two' do
+          expect(
+            War < Module
+          ).to be_nil
+        end
+
+        it '<= will return true if module is the same as other' do
+          expect(
+            War <= War
+          ).to be_true
+        end
+
+        it '<=> returns 1 if the first module is included in the second' do
+          expect(
+            Battles <=> War
+          ).to eq 1
+        end
+
+        it '== returns true if obj and other are same object' do
+          expect(
+            Battles == Battles
+          ).to be_true
+        end
+
+        it '> will return true if module is ancestor of other' do
+          expect(
+            Module > Games
+          ).to be_true
+        end
+
+        it '>= will return true if module is the same as other' do
+          expect(
+            War >= War
+          ).to be_true
+        end
+
+        it 'ancestors returns a array of modules included in the main module' do
+          expect(War.ancestors).to eq [War, Battles]
         end
       end
     end
