ce8b93821707eff19ac0a71511f757a65b436ecb
bomatson <bobby@carbonfive.com>
2013-12-04 13:31:51 -0800

    start public instance methods for Struct, element reference and attribute assignment

--- spec_the_std_lib/rspec/struct_spec.rb
+++ spec_the_std_lib/rspec/struct_spec.rb
@@ -34,4 +34,41 @@ describe Struct do
       expect(Person.new("Roberto")).to be_instance_of Person
     end
   end
+
+  context 'public instance methods' do
+    before(:all) { Folk = Struct.new(:name) }
+    let(:folk) { Folk.new('bob') }
+
+    it '== returns true if other_struct has same values of all instance variables' do
+      expect(
+        folk == Folk.new('bob')
+      ).to be_true
+    end
+
+    it '[:symbol] reference returns value of instance var' do
+      expect(folk[:name]).to eq 'bob'
+    end
+
+    it '[symbol] reference that dne raises NameError' do
+      expect{ folk[:booga] }.to raise_error(NameError)
+    end
+
+    it '[index] reference returns value of instance var' do
+      expect(folk[0]).to eq 'bob'
+    end
+
+    it '[index] reference that dne raises IndexError' do
+      expect{ folk[1] }.to raise_error(IndexError)
+    end
+
+    it '[:symbol]= assigns attribute to variable' do
+      folk[:name] = 'sandy'
+      expect(folk.name).to eq 'sandy'
+    end
+
+    it '[index]= assigns attribute to variable' do
+      folk[0] = 'sandy'
+      expect(folk.name).to eq 'sandy'
+    end
+  end
 end
