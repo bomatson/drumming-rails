b23cbe4d3c3b8a8c7850c7c2b240546bd823eb48
bomatson <bobby@carbonfive.com>
2013-12-09 10:32:43 -0800

    Integer#chr and #ceil

--- spec_the_std_lib/rspec/integer_spec.rb
+++ spec_the_std_lib/rspec/integer_spec.rb
@@ -1,9 +1,19 @@
 require 'rspec'
 
 describe Integer do
-  context 'public .class methods' do
+  context 'public .instance methods' do
+    let(:ascii_char) { ?\xFF }
 
-    it '.new without a symbol will raise ArgmentError' do
+    it '#ceil returns the receiver' do
+      expect(12.ceil).to eq 12
+    end
+
+    it '#chr defaults to ASCII encoding' do
+      expect(255.chr.encoding).to eq Encoding::ASCII_8BIT
+    end
+
+    it '#chr with encoding provides a string containing char of receiver\'s value' do
+      expect(255.chr(Encoding::UTF_8)).to eq "\303\277"
     end
   end
 end
