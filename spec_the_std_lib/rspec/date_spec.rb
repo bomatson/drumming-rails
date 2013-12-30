ebff071b937cf926eb5d340461be4905431a909a
bomatson <bobby@carbonfive.com>
2013-12-30 14:06:23 -0500

    Date.commercial and Date.ordinal

--- spec_the_std_lib/rspec/date_spec.rb
+++ spec_the_std_lib/rspec/date_spec.rb
@@ -4,36 +4,36 @@ describe Date do
   context 'public .class methods' do
     context ' object creation' do
       context 'with .new' do
-        it '.new provides an instance of Date' do
+        it 'provides an instance of Date' do
           expect(Date.new).to be_instance_of Date
         end
 
-        it '.new with no args defaults to 4712 BCE' do
+        it 'given no args defaults to 4712 BCE' do
           expect(Date.new.to_s).to eq '-4712-01-01'
         end
 
-        it '.new with a year defaults to first day of calendar year' do
+        it 'given a year defaults to first day of calendar year' do
           expect(Date.new(2001).to_s).to eq '2001-01-01'
         end
 
-        it '.new with a year, month and day provides that exact date' do
+        it 'given a year, month and day provides that exact date' do
           expect(Date.new(2013, 12, 19).to_s).to eq '2013-12-19'
         end
 
-        it '.new with a negative month treats is as absolute number' do
+        it 'given a negative month treats is as absolute number' do
           expect(Date.new(2013, -1, 19).to_s).to eq '2013-12-19'
         end
 
-        it '.new with a negative day treats is as absolute number' do
+        it 'given a negative day treats is as absolute number' do
           expect(Date.new(2013, 12, -1).to_s).to eq '2013-12-31'
         end
 
-        it '.new with a 0 month raises an Argument error' do
-          expect{ Date.new(2013, 0, 19).to_s }.to raise_error(ArgumentError)
+        it 'given a 0 month raises an Argument error' do
+          expect{ Date.new(2013, 0, 19) }.to raise_error(ArgumentError)
         end
 
-        it '.new with a 0 day raises an Argument error' do
-          expect{ Date.new(2013, 1, 0).to_s }.to raise_error(ArgumentError)
+        it 'given a 0 day raises an Argument error' do
+          expect{ Date.new(2013, 1, 0) }.to raise_error(ArgumentError)
         end
       end
 
@@ -44,6 +44,54 @@ describe Date do
 
         it 'without an argument defaults to the first day of the julian calendar' do
           expect(Date.jd.to_s).to eq '-4712-01-01'
+        end
+      end
+
+      context 'with .ordinal' do
+        it 'given no args defaults to 4712 BCE' do
+          expect(Date.ordinal.to_s).to eq '-4712-01-01'
+        end
+
+        it 'given a year defaults to first day of calendar year' do
+          expect(Date.ordinal(2001).to_s).to eq '2001-01-01'
+        end
+
+        it 'given a year and positive yearday provides that exact date' do
+          expect(Date.ordinal(2001, 44).to_s).to eq '2001-02-13'
+        end
+
+        it 'given a year and negative yearday provides that exact date' do
+          expect(Date.ordinal(2001, -1).to_s).to eq '2001-12-31'
+        end
+
+        it 'given a 0 yearday raises an Argument error' do
+          expect{ Date.new(2000, 0) }.to raise_error(ArgumentError)
+        end
+      end
+
+      context 'with .commercial' do
+        it 'given no args defaults to 4712 BCE' do
+          expect(Date.commercial.to_s).to eq '-4712-01-01'
+        end
+
+        it 'given a calendar week year defaults to first day of calendar year' do
+          expect(Date.commercial(2001).to_s).to eq '2001-01-01'
+        end
+
+        it 'given a year defaults to the first week where that year has Thursday in the week' do
+          expect(Date.commercial(2008).to_s).to eq '2007-12-31'
+        end
+
+        it 'given a week uses the Monday from that calendar week date' do
+          expect(Date.commercial(2008, 3).to_s).to eq '2008-01-14'
+        end
+
+        it 'given a weekday uses the next day from that calendar week date' do
+          expect(Date.commercial(2008, 3, 2).to_s).to eq '2008-01-15'
+        end
+
+        it 'given a 0 weekday raise an ArgumentError' do
+          expect{ Date.commercial(2008, 3, 0).to_s }.to raise_error(ArgumentError)
         end
       end
     end
